750503098f844f6997327236950f53a4b1f69fe0
gaesudal <gaesudal@gmail.com>
2023-05-17 14:27:21 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.02 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i < (n-1)/2 ; i++){
+            if ((n-1)%i == 0) {
+                answer = i;
+                break;
+            }
+        }
+        return answer == 0 ? n-1 : answer;
+    }
+}
